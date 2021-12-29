01050b9e008644336701a61deb58ce1d984ba0c5
kandagatla9 <srinathkandagatla6@gmail.com>
2021-12-29 15:24:40 +0530

    update4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/silicon_heroku'));
+app.use(express.static(__dirname + '/dist/silicon'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/silicon_heroku/index.html'));
+res.sendFile(path.join(__dirname+'/dist/silicon/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
